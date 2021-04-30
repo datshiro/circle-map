481af15f6c387bdbcfe311002d6b3f1648559afe
Dat Shiro <datshiro@gmail.com>
2021-04-30 13:44:19 +0700

    Add apply call api get data and receive params

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # circle-map
+
+
+# To run http server
+## Requirements: yarn installed
+
+## Instructions
+### Step 1: Install packages
+    ```
+        yarn install
+    ```
+    
+### Step 2: Start server
+
+    ```
+        yarn server
+    ```
+
--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 // Initialize and add the map
-function initMap() {
-  const data = [
-    {
-      lat: -25.344,
-      lng: 131.036,
-      value: 300,
-    },
-    {
-      lat: -1.6311362,
-      lng: 116.6358188,
-      value: 1400,
-    },
+//
+const default_url =
+  "http://decisions.truebpm.vn/Primary/?FlowAlias=th_scs_shar_get_map_data&Guest=true&outputtype=RawJson&action=api&fbclid=IwAR2s_LYthBJiUwV53gKY7bcmAbbxJZD_KSeHf79AzSa92FF2dX9mZTY474Y";
 
-    {
-      lat: 10.7603994,
-      lng: 106.6766961,
-      value: 340,
-    },
-    {
-      lat: 120.7603994,
-      lng: 109.6766961,
-      value: 540,
-    },
-  ];
+async function initMap() {
+  const params = parseParam(document.location.search);
+  let url = default_url;
+  if (params.source) {
+    url = params.source;
+  }
 
-  const map_center = new google.maps.LatLng(-2.5521896, 117.9706619);
+  var data;
+  try {
+    const resp = await fetchData(url);
+    data = resp.data;
+  } catch (err) {
+    alert("failed to call api " + url + "\n", err);
+    return;
+  }
+
+  const map_center = new google.maps.LatLng(17.4520535, 102.4301246);
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 5,
     center: map_center,
   });
   data.map((loc) => {
     const circle = new google.maps.Circle({
       map: map,
-      radius: Math.sqrt(loc.value * 2000) * 100,
+      radius: Math.sqrt(loc.data) * 100,
       strokeColor: "#FF0000",
       strokeOpacity: 0.8,
       strokeWeight: 2,
@@ -41,4 +36,29 @@ function initMap() {
       center: new google.maps.LatLng(loc.lat, loc.lng),
     });
   });
+}
+
+function fetchData(url) {
+  return new Promise(function (myResolve, myReject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      var status = xhr.status;
+      if (status === 200) {
+        myResolve(xhr.response);
+      } else {
+        myReject(status);
+      }
+    };
+    xhr.send();
+  });
+}
+
+function parseParam(queryString) {
+  var params = getUrlParams(queryString);
+  return {
+    action: params.get("action"),
+    source: params.get("source"),
+  };
 }
